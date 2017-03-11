7f30f79346304de0ad10d9c93c8c1f9c539991d4
Mehdi Moalla <mehdimoalla2010@gmail.com>
2017-03-11 22:31:36 +0100

    delete cookies file

--- src/config/cookies.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-app.config(['$cookiesProvider', function ($cookiesProvider){
-    $cookiesProvider.defaults.domain = 'angulardemo.alwaysdata.net';
-    $cookiesProvider.defaults.secure = true;
-}]);
